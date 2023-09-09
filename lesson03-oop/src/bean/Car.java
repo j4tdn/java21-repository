4387fea3d9e25d4e8ec829e765dec405633e7c56
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-09-09 20:36:02 +0700

    lesson03-oop 09.09.2023

---
+++ lesson03-oop/src/bean/Car.java
@@ -0,0 +1,15 @@
+package bean;
+
+// pojo: plain old java object
+// Car, Item, Store, Employee: Khai báo KDL của bài toán
+
+// Spring FW: Lưu trữ những đối tượng của class vào vùng nhớ của nó
+//          : Đối tượng --> bean
+
+// DTO
+
+// Entity
+
+public class Car {
+
+}
