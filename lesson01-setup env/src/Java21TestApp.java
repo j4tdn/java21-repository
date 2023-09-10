9ebd3e2f02a3951e8ba76a48f8fb60ec84b38ba6
lethi <volethi30062003@gmail.com>
2023-09-11 00:46:31 +0700

    lesson01-setup env 11.09.2023

---
+++ lesson01-setup env/src/Java21TestApp.java
@@ -0,0 +1,11 @@
+public class Java21TestApp {
+
+    public static void main(String[] agrs) {
+	
+	System.out.println("Hello Java21 class");
+ 	int a = 5;
+	int b = 10;
+	int c = a + b;
+	System.out.println("c --> " + c);
+      }
+}
