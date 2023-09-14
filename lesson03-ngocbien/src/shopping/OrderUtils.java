4983d85dcbd7a94631e099908ac21a87b6b5e654
TranNgocBien <tranngocbien2001@gmail.com>
2023-09-14 15:05:20 +0700

    lesson03-ngocbien 14.09.2023

---
+++ lesson03-ngocbien/src/shopping/OrderUtils.java
@@ -0,0 +1,42 @@
+package shopping;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/*
+ * Utility of Order Class
+ * Normally: Utility is placw where contains utility methods
+ * No cares created class objects
+ */
+
+public class OrderUtils {
+	
+	private OrderUtils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static double export(Order order) {
+		double totolOfMoney = 0;
+		ItemDetail[] ids = order.getItemDetails();
+		// for index
+//		for (int i = 0; i < ids.length; i++) {
+//			ItemDetail id = ids[i];
+//		}
+
+		// for each
+		for (ItemDetail id : ids) {
+			Item item = id.getItem();
+			int quantity = id.getQuantity();
+
+			double idCost = item.getCost() * quantity;
+			if (item.getCost() > 590 && LocalDate.of(2021, Month.MAY, 8).isEqual(order.getOrderDate().toLocalDate())) {
+				idCost *= 0.9;
+			}
+			totolOfMoney += idCost;
+
+		}
+
+		return totolOfMoney;
+
+	}
+}
