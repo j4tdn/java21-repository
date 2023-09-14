ad5f658d39aa10094b99bc5f3f80c3b4ea61f967
hoangvu <hoangvu1195@gmail.com>
2023-09-15 06:29:42 +0700

    lesson03-lhvu 15.09.23

---
+++ lesson03-lhvu/src/shopping/Order.java
@@ -0,0 +1,42 @@
+package shopping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+public class Order {
+
+	private Customer customer;
+	private ItemDetail[] itemDitails;
+	private LocalDateTime datetime;
+	public Order(Customer customer, ItemDetail[] itemDitails, LocalDateTime datetime) {
+		super();
+		this.customer = customer;
+		this.itemDitails = itemDitails;
+		this.datetime = datetime;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public ItemDetail[] getItemDitails() {
+		return itemDitails;
+	}
+	public void setItemDitails(ItemDetail[] itemDitails) {
+		this.itemDitails = itemDitails;
+	}
+	public LocalDateTime getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(LocalDateTime datetime) {
+		this.datetime = datetime;
+	}
+	@Override
+	public String toString() {
+		return "Order [customer=" + customer + ", itemDitails=" + Arrays.toString(itemDitails) + ", datetime="
+				+ datetime + "]";
+	}
+	
+	
+}
---
+++ lesson03-lhvu/src/shopping/OrderUtils.java
@@ -0,0 +1,28 @@
+package shopping;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class OrderUtils {
+	private OrderUtils() {
+	}
+	public static double export(Order order) {
+		double totalOfMoeny = 0;
+			ItemDetail[] ids = order.getItemDitails();
+			 // Duyệt từng phần tử;
+			for (ItemDetail id:ids) {
+				Item item = id.getItem();
+				int quantity = id.getQuantity();
+				double idCost = item.getCost();
+				
+				if(item.getCost()>590 && LocalDate.of(2021, Month.MAY, 8).isEqual(order.getDatetime().toLocalDate())) {
+					idCost*=0.9;
+				} 
+				totalOfMoeny += idCost;
+		
+			}
+		
+		return totalOfMoeny;
+	}
+
+}
