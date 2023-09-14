9aa078ed64b8613d051b2cc86bf3b2a7a5d02dab
TranNgocBien <tranngocbien2001@gmail.com>
2023-09-15 01:11:18 +0700

    Lesson03-ngocbien 15.09.2023

--- lesson03-ngocbien/src/ex02/App.java
+++ lesson03-ngocbien/src/ex02/App.java
@@ -1,10 +1,67 @@
 package ex02;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		 Scanner ip = new Scanner(System.in);
+	     int a;
+	     int n = 0;
+	     Vehicle[] vhc = null;
+	     Boolean exit = true;
+	     
+	     do {
+			System.out.println("1. Nhập thông tin và tạo N đối tượng xe");
+			System.out.println("2. Xuất bản kê khai trước bạ của các xe");
+			System.out.println("3. Thoát chương trình");
+			
+			System.out.print("Lựa chọn (1-3): ");
+			
+			a = Integer.parseInt(ip.nextLine());
+			
+			switch (a) {
+			case 1:
+				System.out.print("Nhập số lượng xe: ");
+				n = Integer.parseInt(ip.nextLine());
+				vhc = new Vehicle[n];
+				
+				for (int i = 0; i < n; i++) {
+					vhc[i] = new Vehicle();
+					System.out.println("Nhập thông tin");
+                    System.out.print("Tên chủ xe: ");
+                    vhc[i].setName(ip.nextLine());
+                    System.out.print("Loại xe: ");
+                    vhc[i].setVehicle(ip.nextLine());
+                    System.out.print("Dung tich: ");
+                    vhc[i].setCapacity(Integer.parseInt(ip.nextLine()));
+                    System.out.print("trị giá: ");
+                    vhc[i].setCost(Double.parseDouble(ip.nextLine()));
+                    System.out.println("\n===\n");
+				}
+				break;
+			case 2:
+				System.out.printf("%-20s %-15s %-15s %-20s %-20s %n", 
+								  "Tên chủ xe", "Loại xe", "Dung tích", "Trị giá", "Thuế phải nộp");
+                System.out.println("===========================================================================");
+                for (Vehicle vhcVehicle : vhc) {
+                    DecimalFormat df = new DecimalFormat("####.00");
+                    System.out.printf("%-20s %-15s %-15s %-20s %-20s %n", 
+                    				  vhcVehicle.getName(), 
+                    				  vhcVehicle.getVehicle(), 
+                    				  vhcVehicle.getCapacity(), 
+                    				  df.format(vhcVehicle.getCost()), 
+                    				  df.format(VehicleUtils.tax(vhcVehicle)));
+                 break;
+                }
+            case 3:
+                 exit = false;
+                 break;
+			}
+		} while (exit);
+	     
 	}
 
 }
--- lesson03-ngocbien/src/ex02/VehicleUtils.java
+++ lesson03-ngocbien/src/ex02/VehicleUtils.java
@@ -1,5 +1,26 @@
 package ex02;
 
+import java.util.Scanner;
+
 public class VehicleUtils {
 
-}
+	private VehicleUtils() {
+
+	}
+
+	public static double tax(Vehicle vehicle) {
+		double tax = 0;
+		if (vehicle.getCapacity() < 100) {
+			tax = vehicle.getCost() * 0.01;
+		} else if (vehicle.getCapacity() <= 200) {
+			tax = vehicle.getCost() * 0.03;
+		} else {
+			tax = vehicle.getCost() * 0.05;
+		}
+		return tax;
+	}
+	
+	
+		
+	}
+
