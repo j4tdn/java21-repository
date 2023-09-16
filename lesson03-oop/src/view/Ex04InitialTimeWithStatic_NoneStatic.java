2d5ecc5f35f130665a09df98d56b036db4afd6b9
hoangvu <hoangvu1195@gmail.com>
2023-09-16 19:33:28 +0700

    lesson03-oop 16.09.2023

--- lesson03-oop/src/view/Ex04InitialTimeWithStatic_NoneStatic.java
+++ lesson03-oop/src/view/Ex04InitialTimeWithStatic_NoneStatic.java
@@ -1,5 +1,29 @@
 package view;
 
 public class Ex04InitialTimeWithStatic_NoneStatic {
-
+	//attributes, method
+	// static : class loading -- gọi được static khác, không gọi được non static,
+	//non-static: object creating time gọi được non staitic và static
+	//order time: static --> non-staic
+	 private static String staticVar;
+	 
+	 private String nonestaticVar;
+	 
+	public static void main(String[] args) {
+		
+	}
+		 private static void staticMethod() {
+			 
+			 System.out.println("Ex04 --> Static Method");
+	}
+		 private void nonstaticMethod() {
+			 System.out.println(staticVar);
+			 System.out.println(nonestaticVar);
+			 staticMethod();
+			 
+			 
+			 
+			 System.out.println("Ex04 -->  nonstaticMethod");
+		 }
 }
+
