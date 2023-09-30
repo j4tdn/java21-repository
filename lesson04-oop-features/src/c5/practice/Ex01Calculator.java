dbcee3c4b3ef2558ab3d02552bed88d4f0b1232d
hblinh <hbl1103pkq@gmail.com>
2023-09-30 09:27:57 +0700

    lesson04-oop-features 30.09.2023

---
+++ lesson04-oop-features/src/c5/practice/Ex01Calculator.java
@@ -0,0 +1,59 @@
+package c5.practice;
+
+public class Ex01Calculator {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		
+		System.out.println("sum " + sum(a, b));
+		System.out.println("sum strategy --> " + calc(a, b, new Strategy() {
+			
+			@Override
+			public int process(int x, int y) {
+				return x + y;
+			}
+		}));
+		
+		System.out.println("sub " + sub(a, b));
+		
+		System.out.println("mul " + mul(a, b));
+		/*Strategy mulStrategy = new Strategy() {
+			
+			@Override
+			public int process(int x, int y) {
+				return x*y;
+			}
+		};*/
+		
+           Strategy mulStrategy = (int x, int y) ->   {
+        	   return x*y;
+           };
+	
+		System.out.println("mul strategy --> " + calc(a, b, mulStrategy));
+		
+		System.out.println("div " + div(a, b));
+
+		Strategy divStrategy = (x,y) -> x/y;
+		System.out.println("div strategy --> " + calc(a, b, divStrategy));
+	}
+	
+	private static int calc(int a, int b, Strategy strategy) {
+		return strategy.process(a, b);
+	}
+	
+	private static int sum(int a, int b) {
+		return a+b;
+	}
+	
+	private static int sub(int a, int b) {
+		return a-b;
+	}
+	
+	private static int mul(int a, int b) {
+		return a*b;
+	}
+	
+	private static int div(int a, int b) {
+		return a/b;
+	}
+}
---
+++ lesson04-oop-features/src/c5/practice/Strategy.java
@@ -0,0 +1,7 @@
+package c5.practice;
+
+@FunctionalInterface
+public interface Strategy {
+	int process(int a, int b);
+	
+}
