3685a521076fa6e2e9a82859fc378ab821c6dfd6
hblinh <hbl1103pkq@gmail.com>
2023-09-30 10:11:11 +0700

    lesson04-hblinh 30.09.2023

--- lesson01-setup-env/src/Java21AloApp.java
+++
@@ -1,7 +0,0 @@
-public class Java21TestApp {
-
-	public static void main(String[] abc) {
-		System.out.println("Hello Java21 Class");
-	}	
-
-}
--- lesson02-data-structure/src/common/Ex01Final.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class Ex01Final {
-	public static void main(String[] args) {
-		int a = 15;
-		System.out.println(a);
-	}
-}
