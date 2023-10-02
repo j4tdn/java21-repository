3e307bc0ef165f66dd7789eb4b37caa2418b6ac4
thanhvu <thanhvuqt48@gmail.com>
2023-10-02 14:17:11 +0700

    lesson04-thanhvu 2.10.2023

--- lesson04-thanhvu/src/ex04quanlynhansu/Main.java
+++ lesson04-thanhvu/src/ex04quanlynhansu/Main.java
@@ -2,10 +2,13 @@ package ex04quanlynhansu;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
+		List<Staff> arr = new ArrayList<>();
 		Director dr = new Director("Pham Thanh Vu", LocalDate.of(2003, 8, 4), BigDecimal.valueOf(100), BigDecimal.valueOf(25));
 		System.out.println("--> Director: ");
 		System.out.println(dr);
@@ -18,10 +21,14 @@ public class Main {
 		Manager m2 = new Manager("Nguyen Thi Lan", LocalDate.of(1968, 12, 5), BigDecimal.valueOf(80), BigDecimal.valueOf(20), "Take care of customers");
 		System.out.println("--> Manager: ");
 		System.out.println(m1);
+		Management.countStaff(m1, arr);
 		System.out.println("Salary of Manager " + m1.getName() + ": " + m1.countSalary());
+		
 		System.out.println("--------------------------------------------------------");
 		System.out.println(m2);
 		System.out.println("Salary of Manager " + m2.getName() + ": " + m2.countSalary());
+		Management.countStaff(m2, arr);
+		
 		System.out.println("--------------------------------------------------------");
 
 		System.out.println("\n==========================================================================\n");
@@ -36,21 +43,38 @@ public class Main {
 		System.out.println("--> Staff: ");
 		System.out.println(s1);
 		System.out.println("Salary of Staff " + s1.getName() + ": " + s1.countSalary());
+		Management.addStaff(s1, arr);
+		
 		System.out.println("--------------------------------------------------------");
+		
 		System.out.println(s2);
 		System.out.println("Salary of Staff " + s2.getName() + ": " + s2.countSalary());
+		Management.addStaff(s2, arr);
+		
 		System.out.println("--------------------------------------------------------");
+		
 		System.out.println(s3);
 		System.out.println("Salary of Staff " + s3.getName() + ": " + s3.countSalary());
+		Management.addStaff(s3, arr);
+		
 		System.out.println("--------------------------------------------------------");
+		
 		System.out.println(s4);
 		System.out.println("Salary of Staff " + s4.getName() + ": " + s4.countSalary());
+		Management.addStaff(s4, arr);
+		
 		System.out.println("--------------------------------------------------------");
+		
 		System.out.println(s5);
 		System.out.println("Salary of Staff " + s5.getName() + ": " + s5.countSalary());
+		Management.addStaff(s5, arr);
+		
 		System.out.println("--------------------------------------------------------");
+		
 		System.out.println(s6);
 		System.out.println("Salary of Staff " + s6.getName() + ": " + s6.countSalary());
+		Management.addStaff(s6, arr);
+		
 		System.out.println("--------------------------------------------------------");
 
 		System.out.println("\n==========================================================================\n");
---
+++ lesson04-thanhvu/src/ex04quanlynhansu/Management.java
@@ -0,0 +1,22 @@
+package ex04quanlynhansu;
+
+import java.util.List;
+
+public class Management {
+
+	public static void addStaff(Staff st, List<Staff> arr) {
+		arr.add(st);
+	}
+	
+	public static void countStaff(Manager mn, List<Staff> arr) {
+		int count = 0;
+		for (Staff st : arr) {
+			if (st.getManager().getName().equalsIgnoreCase(mn.getName())) {
+				count++;
+			}
+		}
+		System.out.println("The number staff of manager: " + count);
+	}
+	
+
+}
