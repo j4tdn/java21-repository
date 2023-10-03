d2816fd843a5b1ce20a6dd48a23d168c5b97c99e
TranNgocBien <tranngocbien2001@gmail.com>
2023-10-03 21:28:23 +0700

    lesson05-ngocbien 03.10.2023

--- lesson05-ngocbien/src/ex04/Ex04.java
+++ lesson05-ngocbien/src/ex04/Ex04.java
@@ -5,12 +5,12 @@ public class Ex04 {
 	public static void main(String[] args) {
 		int[] a = {1, 5, 8, 9, 2, 5, 9};
 		for (int i = 0; i < a.length; i++) {
-			if (findNumberTheSame(a, a[i]))
-				System.out.print( a[i]);
+			if (findNumberTheSame(a, a[i]) == 1)
+				System.out.println( a[i]);
 		}
 	}
 	
-	private static findNumberTheSame(int[] a, int i) {
+	private static int findNumberTheSame(int[] a, int i) {
 		int count = 0;
 		for (int j = 0; j < 7; j++) {
 			if (a[j] == i)
