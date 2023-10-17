d705cf4679b6a52e12838933d879de9bf332b026
tungf20t3 <20t3.luuvantung@gmail.com>
2023-10-17 19:35:13 +0700

    lesson06-lvtung 15.10.2023

---
+++ lesson06-lvtung/src/homework/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package homework.exception;
+
+public class ValidationException extends Exception{
+
+	private static final long serialVersionUID = -1944335309322020845L;
+	
+	public ValidationException(String message) {
+		super(message);
+	}
+
+}
