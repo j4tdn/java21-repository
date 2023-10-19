ae767db5f31a5e8c240bc61f2c77bd9040c80a11
ador0312 <dor.a032294@gmail.com>
2023-10-19 21:09:38 +0700

    lesson06-ador 19.10.2023

---
+++ lesson06-ador/src/test02/VerifyException.java
@@ -0,0 +1,12 @@
+package test02;
+
+public class VerifyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -643491700477029866L;
+	public VerifyException(String message) {
+		super(message);
+	}
+}
---
+++ lesson06-ador/src/test04/VerifyNumException.java
@@ -0,0 +1,13 @@
+package test04;
+
+public class VerifyNumException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3021902279005330472L;
+	
+	public VerifyNumException(String message) {
+		super(message);
+	}
+}
