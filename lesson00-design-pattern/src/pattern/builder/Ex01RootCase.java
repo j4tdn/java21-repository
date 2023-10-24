2ebd2dead6f4c38de3a683c9c62a1914f15b8fce
Mai Tran <maimaimaihoabinh@gmail.com>
2023-10-24 16:07:29 +0700

    UNDONE

---
+++ lesson00-design-pattern/src/pattern/builder/Ex01RootCase.java
@@ -0,0 +1,16 @@
+package pattern.builder;
+
+public class Ex01RootCase {
+ /*
+  
+  Design Pattern: Là các mẫu thiết kế, được sinh/tạo ra từ kinh nghiệm của các người đi trước
+                : Giúp mình giải quyết các vấn đề cụ thể
+  
+   
+  Đặt vấn đề
+  
+  ĐƯợc dùng để tạo ra đối tượng cho các complex class/object
+  
+  
+  */
+}
