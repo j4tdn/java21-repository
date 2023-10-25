4da44685a02972b097eebcd4a69bb5b761a6edda
hblinh <hbl1103pkq@gmail.com>
2023-10-26 01:27:17 +0700

    lesson07-hblinh 26.10.2023

--- lesson07-hblinh/src/ex01/Ex01TheUltimateRelationshipCalculator.java
+++ lesson07-hblinh/src/ex01/Ex01TheUltimateRelationshipCalculator.java
@@ -1,3 +1,4 @@
+package ex01;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -31,19 +32,28 @@ public class Ex01TheUltimateRelationshipCalculator {
 				timeNow.add(Calendar.DAY_OF_MONTH, -calendar.get(Calendar.DAY_OF_MONTH));
 				timeNow.add(Calendar.MONTH, -calendar.get(Calendar.MONTH));
 				timeNow.add(Calendar.YEAR,-calendar.get(Calendar.YEAR));
-				System.out.println("Bạn yêu được :" +((timeNow.get(Calendar.YEAR)))+" năm "
-				+(timeNow.get(Calendar.MONTH))+" tháng "+timeNow.get(Calendar.DAY_OF_MONTH)+" ngày");
+				timeNow.add(Calendar.HOUR, -calendar.get(Calendar.HOUR));
+				timeNow.add(Calendar.MINUTE, -calendar.get(Calendar.MINUTE));
+				timeNow.add(Calendar.SECOND,-calendar.get(Calendar.SECOND));
+				System.out.println("Bạn đã yêu được: " +((timeNow.get(Calendar.YEAR)))+" năm "
+				+(timeNow.get(Calendar.MONTH))+" tháng "+timeNow.get(Calendar.DAY_OF_MONTH)+" ngày "
+				+timeNow.get(Calendar.HOUR)+" giờ "+timeNow.get(Calendar.MINUTE)+" phút "+timeNow.get(Calendar.SECOND)+" giây ");
 			} else {
+				sc.nextLine();
 				System.out.println("Nhập thời gian chia tay(dd/MM/yyyy HH:mm:ss)");
 				timeEnd = sc.nextLine();
 				conventerStringtoDate1(timeEnd);
-				Ex01TheUltimateRelationshipCalculator obj1 = new Ex01TheUltimateRelationshipCalculator();
-				Calendar calendar1 = obj1.dateToCalendar(date1);
-				timeEnd.add(Calendar.DAY_OF_MONTH, -calendar1.get(Calendar.DAY_OF_MONTH));
-				timeEnd.add(Calendar.MONTH, -calendar1.get(Calendar.MONTH));
-				timeEnd.add(Calendar.YEAR,-calendar1.get(Calendar.YEAR));
-				System.out.println("Bạn yêu được :" +((timeEnd.get(Calendar.YEAR)))+" năm "
-				+(timeEnd.get(Calendar.MONTH))+" tháng "+timeEnd.get(Calendar.DAY_OF_MONTH)+" ngày");
+				Calendar timeNow1 = Calendar.getInstance();
+				timeNow1.setTime(date1);
+				timeNow1.add(Calendar.DAY_OF_MONTH, -calendar.get(Calendar.DAY_OF_MONTH));
+				timeNow1.add(Calendar.MONTH, -calendar.get(Calendar.MONTH));
+				timeNow1.add(Calendar.YEAR,-calendar.get(Calendar.YEAR));
+				timeNow1.add(Calendar.HOUR, -calendar.get(Calendar.HOUR));
+				timeNow1.add(Calendar.MINUTE, -calendar.get(Calendar.MINUTE));
+				timeNow1.add(Calendar.SECOND,-calendar.get(Calendar.SECOND));
+				System.out.println("Bạn đã yêu được: " +((timeNow1.get(Calendar.YEAR)))+" năm "
+				+(timeNow1.get(Calendar.MONTH))+" tháng "+timeNow1.get(Calendar.DAY_OF_MONTH)+" ngày "
+				+timeNow1.get(Calendar.HOUR)+" giờ "+timeNow1.get(Calendar.MINUTE)+" phút "+timeNow1.get(Calendar.SECOND)+" giây ");
 			}
 			
 		
@@ -60,5 +70,6 @@ public class Ex01TheUltimateRelationshipCalculator {
 	public static void conventerStringtoDate1(String timeEnd) throws Exception {
 	       date1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(timeEnd);        
 	}
+	
 	
 }
