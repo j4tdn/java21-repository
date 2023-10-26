591e64e31b49294bd3e98b734e75234a6744fe04
hoangvu <hoangvu1195@gmail.com>
2023-10-27 06:19:00 +0700

    lesson07-24.10.2023

---
+++ lesson07-library/src/homework/Ex01.java
@@ -0,0 +1,14 @@
+package homework;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Ex01 {
+	 public static void main(String[] args) {
+		
+	
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("Nhập ngày bắt đầu:");
+	      
+}
