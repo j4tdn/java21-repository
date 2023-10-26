4661246be876b80089a13b172f969c30808b56da
Mai Tran <maimaimaihoabinh@gmail.com>
2023-10-26 22:07:07 +0700

    lesson07-maitran 26.10.2023

--- lesson07-maitran/src/version/java8/Ex01UltimateRelationshipCal.java
+++ lesson07-maitran/src/version/java8/Ex01UltimateRelationshipCal.java
@@ -1,9 +1,10 @@
 package version.java8;
 
-import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import utils.DateUtils;
@@ -13,15 +14,13 @@ public class Ex01UltimateRelationshipCal {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("\n---------------------------------------------------------------");
 		System.out.println("| Nếu không nhập Giờ:Phút:Giây, hệ thống sẽ mặc định 00:00:00 |");
 		System.out.println("---------------------------------------------------------------");
-		System.out.print("Nhập thời gian bắt đầu hẹn hò (dd/MM/yyyy HH:mm:ss): ");
-		String userInput = sc.nextLine();
-		sc.close();
-		if (!userInput.contains(":")) {
-			userInput += " 00:00:00";
-		}
+
+		String userInput = promptForDateTime(sc, "Nhập ngày bắt đầu hẹn hò: ");
+
 		LocalDateTime sDate = DateUtils.toDate(userInput, DEFAULT_PATTERN);
 		LocalDateTime eDate = LocalDateTime.now();
 
@@ -29,21 +28,39 @@ public class Ex01UltimateRelationshipCal {
 			throw new IllegalArgumentException("Date range is invalid");
 		}
 
-		System.out.println("Thời gian bắt đầu hẹn hò: " + sDate.getDayOfMonth() + "/" + sDate.getMonthValue() + "/"
-				+ sDate.getYear());
+		Map<String, Runnable> actions = new HashMap<>();
+		actions.put("Y", () -> calculateRelationship(sDate, eDate));
+		actions.put("N", () -> {
+			String breakInput = promptForDateTime(sc, "Nhập thời gian chia tay: ");
+			LocalDateTime breakDate = DateUtils.toDate(breakInput, DEFAULT_PATTERN);
+			calculateRelationship(sDate, breakDate);
+		});
 
-		// Thời gian bắt đầu hẹn hò thứ mấy?
-		DayOfWeek dateDayOfWeek = sDate.getDayOfWeek();
-		System.out.println("Ngày bắt đầu hẹn hò là thứ: " + dateDayOfWeek);
+		System.out.println("Bạn vẫn còn trong mối quan hệ chứ? (Y/N)");
+		String choice;
+		do {
+			choice = sc.nextLine().toUpperCase();
+			actions.getOrDefault(choice, () -> System.out.println("Lựa chọn không hợp lệ! Vui lòng chọn 'Y' hoặc 'N'."))
+					.run();
+		} while (!actions.containsKey(choice));
 
-		// Thời gian yêu nhau bao lâu rồi?
+		sc.close();
+	}
+
+	private static String promptForDateTime(Scanner sc, String text) {
+		String userInput = sc.nextLine();
+		userInput = userInput.contains(":") ? userInput : userInput + " 00:00:00";
+		return userInput;
+	}
+
+	private static void calculateRelationship(LocalDateTime sDate, LocalDateTime eDate) {
 		Period period = Period.between(sDate.toLocalDate(), eDate.toLocalDate());
 		int years = period.getYears();
 		int months = period.getMonths();
 		int days = period.getDays();
 
 		Duration duration = Duration.between(sDate, eDate);
-		long hours = duration.toHoursPart(); //4 năm 0 tháng 0 ngày 9 giờ 15 phút 33 giây 
+		long hours = duration.toHoursPart();
 		long minutes = duration.toMinutesPart();
 		long seconds = duration.toSecondsPart();
 		System.out.println("Các bạn đã yêu nhau được: " + years + " năm " + months + " tháng " + days + " ngày " + hours
