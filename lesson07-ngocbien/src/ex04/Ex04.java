c297b984f35baaaf4259a0c71b38c8086d4cc789
TranNgocBien <tranngocbien2001@gmail.com>
2023-10-29 17:36:30 +0700

    lesson07-ngocbien 29.10.2023

--- lesson07-ngocbien/src/ex04/Ex04.java
+++ lesson07-ngocbien/src/ex04/Ex04.java
@@ -5,32 +5,35 @@ import java.time.LocalDate;
 import java.time.Month;
 
 public class Ex04 {
-	
+
 	public static void main(String[] args) {
-		
+
 		LocalDate startDate = LocalDate.of(2022, Month.JUNE, 7);
 		int days = 110;
-		
+
 		System.out.println("Ngày nhận dự án: " + startDate);
 		System.out.println("Ngày bàn giao sản phẩm: " + deadline(startDate, days));
-		
+
 	}
-	
+
 	public static LocalDate deadline(LocalDate startDate, int days) {
 		int daysRemainning = days;
 		LocalDate getProject = startDate;
 		int count = 0;
-		
-		while(daysRemainning > 0 ) {
+
+		while (daysRemainning > 0) {
 			getProject = getProject.plusDays(1);
 			daysRemainning--;
-			
-			if(getProject.getDayOfWeek().equals(DayOfWeek.SATURDAY) || getProject.getDayOfWeek().equals(DayOfWeek.SUNDAY)){
+
+			if (getProject.getDayOfWeek().equals(DayOfWeek.SATURDAY)
+					|| getProject.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
 				count++;
 			}
 		}
-		
+
 		getProject = getProject.plusDays(count);
 		return getProject;
+
 	}
+
 }
