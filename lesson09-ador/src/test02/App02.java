74b29977a51f0820c209b59b52b97418bb05639a
ador0312 <dor.a032294@gmail.com>
2023-11-11 21:51:44 +0700

    lesson09-ador 11.11.2023

--- lesson09-ador/src/test02/App02.java
+++ lesson09-ador/src/test02/App02.java
@@ -2,6 +2,42 @@ package test02;
 
 public class App02 {
 	public static void main(String[] args) {
+		String yourInput = "aa6b546c6e22h, aa6b326c6e22h";
+		String[] findNum = yourInput.strip().split(", ");
+		getLargestNumber(findNum);
 		
+	}
+	private static void getLargestNumber(String[] yourString) {
+		long[] arr = new long[20];
+		System.out.println("Kết quả: ");
+		for(String str:yourString) {
+			arr = getNumInArr(str);
+			System.out.print(largestNum(arr) + ", ");
+		}
+	}
+	private static long[] getNumInArr(String st) {
+		long[] arr = new long[st.length()];
+		String toNum = "0";
+		int index = 0;
+		for(int i = 0; i < st.length(); i++) {
+			String ch = st.charAt(i) + "";
+			try {
+				toNum += Long.parseLong(ch) + "";
+			}catch (NumberFormatException e) {
+				arr[index] = Long.parseLong(toNum);
+				index++;
+			}
+		}
+		return arr;
+	}
+	private static long largestNum(long[] arr) {
+		long max = arr[0];  
+        //Loop through the array  
+        for (int i = 0; i < arr.length; i++) {  
+            //Compare elements of array with max  
+           if(arr[i] > max)  
+               max = arr[i];  
+        }
+        return max;
 	}
 }
