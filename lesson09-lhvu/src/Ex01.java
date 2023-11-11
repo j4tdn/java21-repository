a42be983dabe6e875d1b8c4a8a7dbff26bdab176
hoangvu <hoangvu1195@gmail.com>
2023-11-11 21:46:34 +0700

    lesson09-lhvu 11.11.2023

--- lesson09-lhvu/src/Ex01.java
+++ lesson09-lhvu/src/Ex01.java
@@ -1,4 +1,22 @@
 
 public class Ex01 {
-
+		//Bài 1(30đ): Trong năm 2021 có N sinh viên khoa CNTT đăng ký học phần cấu trúc dữ liệu. Thông
+		//tin mỗi sinh viên được lưu trữ tại hệ thống gồm: Mã sinh viên, Họ tên, Xếp loại
+		//Xếp loại học lực được lưu trong Enum với các giá trị: A, B, C, D, E, F
+		//Với sinh viên có học lực loại F bắt buộc phải đăng ký học lại.
+		//Student[] students
+		//{
+		//new Student(102, "Nam", C),
+		//new Student(103, "Bảo", F),
+		//new Student(104, "Hoàng", D),
+		//new Student(105, "Nguyên", B),
+		//new Student(107, "Vũ", F),
+		//new Student(109, "Lan", A),
+		//new Student(202, "Đạt", C),
+		//new Student(103, "Bảo", F),
+		//new Student(107, "Vũ", C),
+		//new Student(104, "Hoàng", B)
+		//}
+		// Tìm danh sách sinh viên từng đạt loại A trong năm 2021.
+		// Tìm danh sách sinh viên đã tham gia học lại môn cấu trúc dữ liệu trong năm 2021.
 }
--- lesson09-lhvu/src/Ex04.java
+++ lesson09-lhvu/src/Ex04.java
@@ -1,6 +1,6 @@
-	import java.awt.dnd.InvalidDnDOperationException;
-	import java.util.Scanner;
-	import javax.management.RuntimeErrorException;
+import java.awt.dnd.InvalidDnDOperationException;
+import java.util.Scanner;
+
 public class Ex04 {
 
 	/*
@@ -13,66 +13,62 @@ public class Ex04 {
 	 * báo đăng ký tài khoản thành công
 	 */
 
+	public static void main(String[] args) {
 
-		public static void main(String[] args) {
-			
-			Scanner ip = new Scanner(System.in);
-			
-			do {
-				try {System.out.print("Enter your password: ");
-					String password = ip.nextLine();
-					System.out.print("Enter your ID: ");
-					String id = ip.nextLine();
-					passWordValid(password, id);
-					break;
-					
-				} catch (ArithmeticException e) {
-					System.out.println(e.getMessage());
-					System.out.println("Please enter your password again:");
-				}
-			} while (true);
-			
+		Scanner ip = new Scanner(System.in);
+
+		do {
+			try {
+				System.out.print("Enter your password: ");
+				String password = ip.nextLine();
+				System.out.print("Enter your ID: ");
+				String id = ip.nextLine();
+				passWordValid(password, id);
+				break;
+
+			} catch (ArithmeticException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Please enter your password again:");
+			}
+		} while (true);
+
+		ip.close();
+		System.out.println("Your password is created success");
+
+	}
+
+	private static void passWordValid(String password, String id) throws RuntimeException {
+		if (password.length() < 8 || password.length() > 256) {
+			throw new ArithmeticException(
+					"Độ dài mật khẩu phải lớn hơn 8 ký tự!" + "\nCó ít nhất 1 ký tự viết hoa!" + "\nCó ít nhất 1 số!"
+							+ "\nCó ít nhất 1 ký tự đặc biệt!" + "\nKhông được trùng quá với tên tài khoản");
+		}
+		if (!password.matches(".*[A-Z].*")) {
+			throw new ArithmeticException(
+					"Độ dài mật khẩu phải lớn hơn 8 ký tự!" + "\nCó ít nhất 1 ký tự viết hoa!" + "\nCó ít nhất 1 số!"
+							+ "\nCó ít nhất 1 ký tự đặc biệt!" + "\nKhông được trùng quá với tên tài khoản");
+		}
+		if (!password.matches(".*\\d.*")) {
+			throw new ArithmeticException(
+					"Độ dài mật khẩu phải lớn hơn 8 ký tự!" + "\nCó ít nhất 1 ký tự viết hoa!" + "\nCó ít nhất 1 số!"
+							+ "\nCó ít nhất 1 ký tự đặc biệt!" + "\nKhông được trùng quá với tên tài khoản");
+		}
+		if (!password.matches(".*\\W.*")) {
+			throw new ArithmeticException(
+					"Độ dài mật khẩu phải lớn hơn 8 ký tự!" + "\nCó ít nhất 1 ký tự viết hoa!" + "\nCó ít nhất 1 số!"
+							+ "\nCó ít nhất 1 ký tự đặc biệt!" + "\nKhông được trùng quá 3 ký tự với tên tài khoản");
+		}
 		
-			ip.close();
-			System.out.println("Your password is created success");
-			
-		}
-		private static void passWordValid (String password, String id) throws RuntimeException {
-			if (password.length()< 8 || password.length()>256) {
-				throw new ArithmeticException("Độ dài mật khẩu phải lớn hơn 8 ký tự!"
-						+"\nCó ít nhất 1 ký tự viết hoa!"
-						+"\nCó ít nhất 1 số!"
-						+"\nCó ít nhất 1 ký tự đặc biệt!"
-						+"\nKhông được trùng quá với tên tài khoản");
+		int count = 0;
+		for(int i = 0; i < password.length(); i++) {
+			String ch = password.charAt(i) + "";
+			if(id.contains(ch)) {
+				count++;
 			}
-			if (!password.matches(".*[A-Z].*")) {
-				throw new ArithmeticException("Độ dài mật khẩu phải lớn hơn 8 ký tự!"
-						+"\nCó ít nhất 1 ký tự viết hoa!"
-						+"\nCó ít nhất 1 số!"
-						+"\nCó ít nhất 1 ký tự đặc biệt!"
-						+"\nKhông được trùng quá với tên tài khoản");
-			}
-			if (!password.matches(".*\\d.*")) {
-				throw new ArithmeticException("Độ dài mật khẩu phải lớn hơn 8 ký tự!"
-						+"\nCó ít nhất 1 ký tự viết hoa!"
-						+"\nCó ít nhất 1 số!"
-						+"\nCó ít nhất 1 ký tự đặc biệt!"
-						+"\nKhông được trùng quá với tên tài khoản");
-			}
-			if (!password.matches(".*\\W.*")) {
-				throw new ArithmeticException("Độ dài mật khẩu phải lớn hơn 8 ký tự!"
-						+"\nCó ít nhất 1 ký tự viết hoa!"
-						+"\nCó ít nhất 1 số!"
-						+"\nCó ít nhất 1 ký tự đặc biệt!"
-						+"\nKhông được trùng quá với tên tài khoản");
-			}
-			if (id.contains(password)) {
-				throw new ArithmeticException("Độ dài mật khẩu phải lớn hơn 8 ký tự!"
-						+"\nCó ít nhất 1 ký tự viết hoa!"
-						+"\nCó ít nhất 1 số!"
-						+"\nCó ít nhất 1 ký tự đặc biệt!"
-						+"\nKhông được trùng quá với tên tài khoản");
+			if(count >= 3) {
+				throw new ArithmeticException("Không được trừng quá 3 ký tự với tên tài khoản !!!");
 			}
 		}
-	
+	}
+
 }
