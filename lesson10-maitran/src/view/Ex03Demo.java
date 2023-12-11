6f4095193c49015be89eb42e4c0f2a409c5daca0
Mai Tran <maimaimaihoabinh@gmail.com>
2023-12-11 13:35:25 +0700

    lesson10-maitran 11.12.2023

--- lesson10-maitran/src/view/Ex03Demo.java
+++ lesson10-maitran/src/view/Ex03Demo.java
@@ -1,5 +1,17 @@
 package view;
 
-public class Ex03Demo {
+import java.util.Arrays;
 
+import bean.Item;
+import model.DataModel;
+
+public class Ex03Demo {
+	public static void main(String[] args) {
+		Item[] items = DataModel.mockItems();
+
+		Item itemWithMaxPrice = Arrays.stream(items).max((i1, i2) -> i1.getSalesPrice().compareTo(i2.getSalesPrice()))
+				.get();
+
+		System.out.println("\nHighest price item: \n" + itemWithMaxPrice);
+	}
 }
