cbcbf2b84370b4fb3fb530b448728584eecf1eb4
thanhvu <thanhvuqt48@gmail.com>
2024-01-13 16:29:05 +0700

    lesson12-stream 13.01.2024

--- lesson12-stream/src/view/Ex01StrategyPattern_Review.java
+++ lesson12-stream/src/view/Ex01StrategyPattern_Review.java
@@ -1,7 +1,7 @@
 package view;
 
-import static utils.CollectionUtils.generate;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 import bean.Apple;
@@ -13,65 +13,87 @@ import static utils.CollectionUtils.*;
 public class Ex01StrategyPattern_Review {
 	
 	public static void main(String[] args) {
-		
 		var inventory = DataModel.getApples();
 		
 		// filter all green apples in inventory
 		
 		// Cách 1: Implementation class
-		var greenApples = filter(inventory, new GreenApplePredicate());
+		var greenApples = filterApples(inventory, new GreenApplePredicate());
 		generate("1. Green Apples(use implementation class)", greenApples);
 		
 		// Cách 2: Anonymous class
-		greenApples = filter(inventory, new Predicate<Apple>() {
-
+		greenApples = filterApples(inventory, new Predicate<Apple>() {
+			
 			@Override
 			public boolean test(Apple apple) {
 				return "green".equals(apple.getColor());
 			}
-		});
-		
+		});		
 		generate("2. Green Apples(use anonymous class)", greenApples);
 		
 		/*
 		 Cách 3: Anonymous function = Lambda expression
 		 Là đoạn code override hàm trừu tượng bên trong functional interface
-		 nhưng mà không có tên hàm
+		 nhưng mà ko có tên hàm
 		 cấu trúc: 
 		 (danh sách tham số của hàm trừu tượng) -> {
-		     // body, override hàm trừu tượng
+			    body, override hàm trừu tượng	
 		 }
 		 
 		 Lưu ý:
-		  + tham số của lambda --> ko yêu cầu phải điền KDL
-		  + nếu chỉ có 1 tham số --> có thể bỏ luôn (...)
-		  + nếu body chỉ có 1 dòng --> bỏ luôn {}, bỏ luôn ;
-		    . nếu return type !void --> bỏ luôn chữ return
-		 */
-		greenApples = filter(inventory, apple -> "green".equals(apple.getColor()));
+		 + tham số của lambda --> ko yêu cầu phải điền KDL
+		 + nếu chỉ có 1 tham số --> có thể bỏ luôn (...)
+		 + nếu body chỉ có 1 dòng --> bỏ luôn {}, bỏ luôn ;
+		   . nếu return type !void --> bỏ luôn chữ return
+		*/
+		greenApples = filterApples(inventory, (Apple apple) -> {
+			return "green".equals(apple.getColor());
+		});
 		generate("3. Green Apples(use anonymous function)", greenApples);
 		
+		
 		// filter all red apples and weight heavier than 150g
-		var redAndHeavyApples = filter(inventory, apple -> "green".equals(apple.getColor()) && apple.getWeight() > 150d);
+		var redAndHeavyApples = filterApples(inventory, (Apple apple) -> {
+			return "red".equals(apple.getColor()) && apple.getWeight() > 400;
+		});
 		generate("4. Red and Heavy Apples", redAndHeavyApples);
 		
-		// filter all apples from Viet Nam
-		var AppleVN = filter(inventory, apple -> "vietnam".equalsIgnoreCase(apple.getCountry()));
-		generate("5. All apple from Viet Nam", AppleVN);
+		// filter all apples from viet nam
+		var vnApples = filter(inventory, apple -> 
+			"vietnam".equalsIgnoreCase(apple.getCountry())
+		);
+		generate("5. Viet Nam Apples", vnApples);
+		
 		
 		// Lambda là gì ?
 		
-		// Lambda là cách viết ngắn gọn để tạo ra giá trị/đối tượng cho biến KDL
-		// functional interface
+		// Lambda(anonymous function) là cách viết ngắn gọn để tạo ra giá trị/đối tượng
+		// cho biến KDl functional interface
 		
-		// Dùng lambda khi nào ?
+		// Dùng lambda chỗ nào ?
 		
-		// Chỗ nào cần khởi tạo funciton interface -> dùng lambda cho code ngắn gọn thay vì 
-		// phải dùng anoymous class | implementation class
+		// Chỗ nào cần khởi tạo function interface -> dùng lambda cho code ngắn gọn thay vì
+		// phải dùng anonymous class | implementation class
 		
-		// Vì sao lambda chỉ khởi tạo đc cho functional interface thôi, interface bình thường có được ko ?
-		// không, vì lambda (hàm ko tên) override hàm trừu tượng trong interface ko có tên
-		// nếu interface có nhiều 1 hàm trừu tượng, nếu các hàm trừu tượng đó có cùng số 
+		// Vì sao lambda chỉ khởi tạo dc cho function interface thôi, interface bình thường dc ko ?
+		
+		// không, vì lambda(hàm ko tên) override hàm trừu tượng trong interface ko có tên
+		// nếu interface có nhiều 1 àm trừu tượng, nếu các hàm trừu tượng đó có cùng số lượng tham số, KD
+		// lúc override sẽ ko biết đang override cho hàm nào
+		
+		// Predicate<T>
+		// boolean test(T t);
+		// boolean isTrue(T t);
+		
+		// Predicate<Apple> pre = (apple) -> true;
+		
+		// Functional interface là gì ?
+		
+		// Là interface chỉ có 1 abstract method
+		
+		// Dùng FI chỗ nào
+		
+		// Thường dùng khi các đoạn code có dạng strategy pattern
 	}
 	
 	// công thức chung của các điều kiện: boolean method(Apple apple)
@@ -79,5 +101,16 @@ public class Ex01StrategyPattern_Review {
 	
 	// Java có sẵn: Predicate<T>
 	
+	private static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> predicate) {
+		var result = new ArrayList<Apple>();
+		
+		for (var apple: inventory) {
+			if (predicate.test(apple)) {
+				result.add(apple);
+			}
+		}
+		
+		return result;
+	}
 	
 }
--- lesson12-stream/src/view/Ex02LambdaSyntax.java
+++ lesson12-stream/src/view/Ex02LambdaSyntax.java
@@ -6,32 +6,55 @@ import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class Ex02LambdaSyntax {
-
+	
 	public static void main(String[] args) {
-
+		
 		// Khi code cho 1 lambda(đúng)
 		// --> suy ra được công thức của "HÀM TRỪU TƯỢNG" của KDL trả về functional interface
 		
-		Runnable runnbable = () -> {}; // void method()
+		Runnable runnable = () -> {}; // --> void method()
 		
-		Supplier<String> supplier = () -> "Raul"; // () -> T
+		// Supplier<String> supplier = () -> "Raul"; // () -> T
+		
+		Supplier<String> supplier = new Supplier<String>() {
+			@Override
+			public String get() {
+				return "Raul";
+			}
+		};
+		String value = supplier.get();
+		
 		Callable<String> callable = () -> "Raul"; // type inference
 		
-		Function<Integer, String> function = (Integer i) -> "Alan" + i; // R method(T t)
+		Function<Integer, String> function = (Integer i) -> "Alan" + i; // R method(T t);
 		
-		System.out.println("apply 15 -> " + function.apply(15));
+		
+		System.out.println("apply 15 --> " + function.apply(15));
+		
+		System.out.println("apply 22 --> " + function.apply(22));
 		
 		UnaryOperator<String> uo = (String s) -> "Iron Man"; // T method(T t)
+		
 		System.out.println("uo --> " + uo.apply("abc"));
+		
 		
 		// Predicate<T>     --> T      -> boolean
 		// Consumer<T>      --> T      -> void
+		// BiConsumer<T, U> --> (T, U)   -> void
 		// Comparator<T>    --> (T, T) -> int
 		// Comparable<T>    --> T      -> int
-		// Funciton<T, R>   --> T      -> R
+		// Function<T, R>   --> T      -> R
 		// UnaryOperator<T> --> T      -> T
 		// Supplier<T>      --> ()     -> T
 		// Callable<T>      --> () throws Exception -> T
-		// Runnale          --> ()     -> void
-	} 
+		// Runnable         --> ()     -> void
+		// IntBinaryOperator --> (int, int) -> int
+		
+		
+	}
+	
+	public static int min(int a, int b) {
+		return a < b ? a : b;
+	}
+	
 }
