f9809f42d31e65d2c8ce41c658abcef2f9d5e320
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-02-21 09:31:49 +0700

    lesson13-files 20.02.2024

---
+++ lesson13-files/src/file/structure/Ex01FileCreator.java
@@ -0,0 +1,15 @@
+package file.structure;
+
+import utils.FileUtils;
+
+public class Ex01FileCreator {
+	
+	public static void main(String[] args) {
+		
+		FileUtils.createNewFile("wdata/winventory.txt");
+		
+		System.out.println("... Finished ...");
+		
+	}
+	
+}
