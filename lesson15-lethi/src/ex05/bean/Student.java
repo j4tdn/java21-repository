de12cab8c48c742895ae2074be3c2f640a21d20e
lethi <volethi30062003@gmail.com>
2024-02-24 23:24:04 +0700

    lesson15-lethi 24.02.2024

--- lesson15-lethi/src/ex05/bean/Student.java
+++ lesson15-lethi/src/ex05/bean/Student.java
@@ -51,7 +51,7 @@ public class Student {
 
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", grade=" + grade + ", gender=" + gender + "]";
+		return "+Student [id=" + id + ", name=" + name + ", grade=" + grade + ", gender=" + gender + "]";
 	}
 	
 	
