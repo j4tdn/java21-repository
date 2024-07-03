a601da5a77ee9de6b9b2192c9f4a0eeaff00552d
Tailoc <taitranhuu08@gmail.com>
2024-07-04 02:48:47 +0700

    lesson17-databse-connection 04.07.24

---
+++ lesson17-databse-connection/src/main/java/connection/DbProvider.java
@@ -0,0 +1,32 @@
+package connection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import constants.DbConstants;
+
+public class DbProvider {
+	
+	private DbProvider() {
+	}
+	
+	public static Properties getProperties() {
+		Properties props = new Properties();
+		
+		try {
+			// Đọc từ đường dẫn tuyệt đối của dự án
+			// FileReader fr = new FileReader(new File(DbConstants.DB_CONFIG_PROPERTIES_PATH));
+			
+			// moved file vào target(run)
+			// đọc từ target = DbProvider.class.getClassLoader()
+			InputStream is = DbProvider.class.getClassLoader()
+					.getResourceAsStream(DbConstants.DB_CONFIG_PROPERTIES_PATH);
+			props.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return props;
+	}
+}
---
+++ lesson17-databse-connection/src/main/java/constants/DbConstants.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class DbConstants {
+	private DbConstants() {
+	}
+	
+	public static final String DB_CONFIG_PROPERTIES_PATH = "config.properties";
+	
+	public static final String DB_KEY_URL = "url";
+	public static final String DB_KEY_USER = "user";
+	public static final String DB_KEY_PASSWORD = "password";
+}
