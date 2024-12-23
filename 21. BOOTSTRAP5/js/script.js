fc99ea6328f4f61a75ffbe351aba1a64b4274a23
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-12-23 08:57:52 +0700

    21. BOOTSTRAP5 23.12.2023

---
+++ 21. BOOTSTRAP5/js/script.js
@@ -0,0 +1,9 @@
+const cwidthLabel = document.querySelector('.cwidth');
+
+window.onload = function() {
+	cwidthLabel.textContent = window.innerWidth;
+}
+
+window.onresize = function() {
+	cwidthLabel.textContent = window.innerWidth;
+}
